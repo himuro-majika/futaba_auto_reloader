a35158201011442b91c639b653a07d6b63d02930
himuro-majika <himuro-majika@users.noreply.github.com>
2016-01-17 13:41:06 +0900

    Bump version to 1.7

--- futaba_auto_reloader.user.js
+++ futaba_auto_reloader.user.js
@@ -6,7 +6,7 @@
 // @include        http://*.2chan.net/*/res/*
 // @include        http://board.futakuro.com/*/res/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
-// @version        1.6.1
+// @version        1.7
 // @grant          GM_addStyle
 // @license        MIT
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAAPUExURYv4i2PQYy2aLUe0R////zorx9oAAAAFdFJOU/////8A+7YOUwAAAElJREFUeNqUj1EOwDAIQoHn/c88bX+2fq0kRsAoUXVAfwzCttWsDWzw0kNVWd2tZ5K9gqmMZB8libt4pSg6YlO3RnTzyxePAAMAzqMDgTX8hYYAAAAASUVORK5CYII=
