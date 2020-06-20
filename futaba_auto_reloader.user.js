4a8fccd2d0ab3a86c0f24303451612a02f2979fb
himuro-majika <himuro-majika@users.noreply.github.com>
2020-06-21 02:43:09 +0900

    Bump version to 1.8.0

--- futaba_auto_reloader.user.js
+++ futaba_auto_reloader.user.js
@@ -7,7 +7,7 @@
 // @include        https://*.2chan.net/*/res/*
 // @include        http://board.futakuro.com/*/res/*
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
-// @version        1.7.1
+// @version        1.8.0
 // @grant          GM_addStyle
 // @grant          GM_xmlhttpRequest
 // @license        MIT
